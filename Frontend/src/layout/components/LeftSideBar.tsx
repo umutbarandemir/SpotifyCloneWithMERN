4ea2dee9c5e7b0215125cae38429a80ecfeaf793
umutbarandemir <59203891+umutbarandemir@users.noreply.github.com>
2025-05-18 17:56:00 +0300

    seed data for songs and albums, sidebar skeleton created,  working on library section

--- Frontend/src/layout/components/LeftSideBar.tsx
+++ Frontend/src/layout/components/LeftSideBar.tsx
@@ -1,11 +1,16 @@
 
+import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
 import { buttonVariants } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import { SignedIn } from "@clerk/clerk-react";
-import { HomeIcon, MessageCircle } from "lucide-react";
+import { HomeIcon, Library, MessageCircle } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const LeftSideBar = () => {
+
+	const isLoading = true;
+
   return (
     	<div className='h-full flex flex-col gap-2'>
 			{/* Navigation menu */}
@@ -43,7 +48,41 @@ const LeftSideBar = () => {
 			</div>
 
             {/* Library section */}
+			<div className='flex-1 rounded-lg bg-zinc-900 p-4'>
+				<div className='flex items-center justify-between mb-4'>
+					<div className='flex items-center text-white px-2'>
+						<Library className='size-5 mr-2' />
+						<span className='hidden md:inline'>Playlists</span>
+					</div>
+				</div>
 
+				<ScrollArea className='h-[calc(100vh-300px)]'>
+					<div className='space-y-2'>
+						{isLoading ? (
+							<PlaylistSkeleton />
+						) : ( <div>Songsssss</div>
+							// albums.map((album) => (
+							// 	<Link
+							// 		to={`/albums/${album._id}`}
+							// 		key={album._id}
+							// 		className='p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer'
+							// 	>
+							// 		<img
+							// 			src={album.imageUrl}
+							// 			alt='Playlist img'
+							// 			className='size-12 rounded-md flex-shrink-0 object-cover'
+							// 		/>
+
+							// 		<div className='flex-1 min-w-0 hidden md:block'>
+							// 			<p className='font-medium truncate'>{album.title}</p>
+							// 			<p className='text-sm text-zinc-400 truncate'>Album • {album.artist}</p>
+							// 		</div>
+							// 	</Link>
+							// ))
+						)}
+					</div>
+				</ScrollArea>
+			</div>
 		</div>
   )
 }
--- README.md
+++ README.md
@@ -7,22 +7,22 @@
 
 ### CLERK
 ``https://dashboard.clerk.com/apps/app_2x2ZzmBWCErxFytr6frGSBjin8a/instances/ins_2x2ZzlaVaiUOpSSQCQMiQkn5icl?app_created=true``-> Website  
-``npx shadcn@latest add card``  
-``npx shadcn@latest add resizable``  
-
-
-
-
-
+  
 ### TAILWIND
 ``npm install tailwindcss @tailwindcss/vite`` -> in the Frontend directry to install tailwind  
 ``import tailwindcss from '@tailwindcss/vite' and   plugins: [tailwindcss()], `` -> Added to vite.config.ts implement tailwind  
-``@import "tailwindcss"; `` -> added this to index.css
+``@import "tailwindcss"; `` -> added this to index.css  
+
 
 ### ZUSTAND
+``npm i zustand``  
 
 ### CHADCN/UI 
 ``https://ui.shadcn.com/docs/installation/vite`` -> installation  
+
+``npx shadcn@latest add card``  
+``npx shadcn@latest add resizable``  
+``npx shadcn@latest add scroll-area``  
 
 # BACKEND
 ``npm init -y``    
