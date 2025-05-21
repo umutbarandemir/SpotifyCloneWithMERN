63b29ebf9220e17be097c3aa625c55b1c9df1764
umutbarandemir <59203891+umutbarandemir@users.noreply.github.com>
2025-05-21 22:33:49 +0300

    albums layout

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from "./pages/Home/Home.tsx";
+import Home from "./pages/home/Home.tsx";
 import AuthCallback from './pages/authCallback/AuthCallback.tsx';
 import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react';
 import MainLayout from './layout/MainLayout.tsx';
 import Chat from './pages/chat/Chat.tsx';
+import Album from './pages/album/Album.tsx';
 
 function App() {
   
@@ -19,6 +20,7 @@ function App() {
       <Route element={<MainLayout />}>
         <Route path='/' element={<Home />} />
         <Route path='/chat' element={<Chat />} />
+        <Route path='/albums/:albumId' element={<Album />} />
       </Route>
     </Routes>
     </>
