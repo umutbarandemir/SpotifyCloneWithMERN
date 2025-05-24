da6e76fe33a8fa50da90cf958e9c501f37793669
umutbarandemir <59203891+umutbarandemir@users.noreply.github.com>
2025-05-24 23:36:56 +0300

    HomePage ui created

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,67 @@
 import Topbar from "@/components/Topbar"
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useMusicStore } from "@/stores/useMusicStore"
+import { Loader } from "lucide-react";
+import { useEffect } from "react";
+import FeaturedSection from "./components/FeaturedSection";
+import SectionGrid from "./components/SectionGrid";
 
 const Home = () => {
+
+  const {featuredSongs,fetchFeaturedSongs,trendingSongs,fetchTrendingSongs,madeForYouSongs,fetchMadeForYou,isLoading} = useMusicStore();
+
+  // Fetch data when the component mounts
+  useEffect(() => {
+    const fetchData = async () => {
+      await Promise.all([
+        fetchFeaturedSongs(),
+        fetchTrendingSongs(),
+        fetchMadeForYou()
+      ]);
+    };
+
+    fetchData();
+  }, [fetchFeaturedSongs, fetchTrendingSongs, fetchMadeForYou]);
+
+  // Determine greeting message based on current hour
+  const currentHour = new Date().getHours();
+  let greetingMessage = "";
+
+  if (currentHour < 12) {
+    greetingMessage = "Good morning";
+  } else if (currentHour < 18) {
+    greetingMessage = "Good afternoon";
+  } else {
+    greetingMessage = "Good evening";
+  }
+
+  
+  // Log the fetched data for debugging
+  // console.log({madeForYou, trendingSongs, featuredSongs});
+
+  // If data is still loading, you can return a loading state or spinner
+	if (isLoading)
+		return (
+			<div className='h-screen w-full flex items-center justify-center'>
+				<Loader className='size-8 text-emerald-500 animate-spin' />
+			</div>
+		);
+    
   return (
-    <div className="rounded-md overflow-hidden">
-      <Topbar/>
-    </div>
+		<main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
+			<Topbar />
+			<ScrollArea className='h-[calc(100vh-180px)]'>
+				<div className='p-4 sm:p-6'>
+					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>{greetingMessage}</h1>
+					<FeaturedSection />
+
+					<div className='space-y-8'>
+						<SectionGrid title='Made For You' songs={madeForYouSongs} isLoading={isLoading} />
+						<SectionGrid title='Trending' songs={trendingSongs} isLoading={isLoading} />
+					</div>
+				</div>
+			</ScrollArea>
+		</main>
   )
 }
 
